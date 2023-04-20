a4b5304d5dceb95d4745ee8ab48d174eb78bd485
Starrah <starrah@foxmail.com>
2023-04-21 00:33:24 +0800

    feat: create the post-install script for patching linuxqq

---
+++ linuxqq-firejail.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+launcher="/usr/share/applications/qq.desktop"
+patched_marker="# This file has been PATCHED by linuxqq-firejail! (https://github.com/Starrah/linuxqq-firejail)"
+flatpak_xdg_utils_dir=""
+
+grep -q "${patched_marker}" "${launcher}"
+if [ $? -eq 0 ]; then
+    echo "${launcher}" "had already been patched, so whe script will not patched it again."
+    return 1
+fi
+
+flatpak_xdg_utils_dir="/usr/lib/flatpak-xdg-utils"
+if [ ! -d "${flatpak_xdg_utils_dir}" ]; then
+    flatpak_xdg_utils_dir="/usr/lib/flatpak-xdg-utils" # try default path 1 (Arch)
+fi
+if [ ! -d "${flatpak_xdg_utils_dir}" ]; then
+    flatpak_xdg_utils_dir="/usr/libexec/flatpak-xdg-utils" # try default path 2 (Ubuntu)
+fi
+if [ ! -d "${flatpak_xdg_utils_dir}" ]; then
+    flatpak_xdg_utils_dir=""
+    echo "Cannot find the path for flatpak-xdg-utils. Please specify the path manually by editing the variable \"flatpak_xdg_utils_dir\" at the beginning of this script."
+    return 1
+fi
+
+set -e
+
+echo "  -> Adding dummy jsbridge handler..."
+mkdir "/usr/share/mime/packages/" -p
+cp $(dirname "$0")"/jsbridge-dummy.desktop" "/usr/share/applications/"
+cp $(dirname "$0")"/jsbridge-dummy.xml" "/usr/share/mime/packages/"
+
+echo "  -> Wrapping launcher..."
+cp "${launcher}" "${launcher}".backup
+sed -i "2s!QQ!QQ in Firejail!" "${launcher}"
+sed -i "\$a""${patched_marker}" "${launcher}"
+sed -i "3s!Exec=!Exec=sh -c \"env PATH=""${flatpak_xdg_utils_dir}"":\$PATH firejail --private-bin=linuxqq,xdg-open,xdg-mime,bash !" "${launcher}"
+sed -i "3s!%U!\"%U!" "${launcher}"
+# rm "${launcher}".backup
+
+echo "  -> Updating desktop and MIME database..."
+update-desktop-database -q
+update-mime-database /usr/share/mime
+
+echo "Success! Now linuxqq will be restricted by firejail when you run from the desktop file (eg. from the start menu)!"
+
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
-The package runs [linuxqq](https://aur.archlinux.org/packages/linuxqq) within patched [Firejail](https://firejail.wordpress.com/).
+## How to use:
+1. Install linuxqq from [the official website](https://im.qq.com/linuxqq/)
+2. Install [Firejail](https://github.com/netblue30/firejail#installing)
+3. Install `xdg-desktop-portal` and `flatpak-xdg-utils`.
+    - eg., for Debian & Ubuntu, just `sudo apt install xdg-desktop-portal flatpak-xdg-utils`
+4. run `sudo sh linuxqq-firejail.sh`
 
-The GitHub repo: https://github.com/BeautyYuYanli/linuxqq-firejail
+**Each time you update or reinstall linuxqq, you must rerun the `linuxqq-firejail.sh`. Or `linuxqq` will no longer be restricted by `firejail`.**
